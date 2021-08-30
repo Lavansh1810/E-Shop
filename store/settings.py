27f71a076805977000674fe11e12cc024c7e0854
Lavansh <aroralavit1810@gmail.com>
2021-08-30 22:50:17 +0530

    Ready to Host

--- store/settings.py
+++ store/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 import os
 
@@ -137,3 +137,6 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
