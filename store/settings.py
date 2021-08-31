12ed3f6bea858d8ca6edd121c4173358664d5c7f
Lavansh <aroralavit1810@gmail.com>
2021-08-31 11:13:26 +0530

    Changes in Settings.py

--- store/settings.py
+++ store/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '$&g*!inv&eq4$voq@kb($fkx6^4vqtaa1u=^s2iloy@&iq9y13'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','allinonemart.herokuapp.com']
+ALLOWED_HOSTS = ['192.168.56.1','allinonemart.herokuapp.com']
 
 
 # Application definition
